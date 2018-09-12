9065c0519a98be0625cdef8d9595646f086f6671
ly989264 <ly989264@163.com>
2018-09-12 10:36:18 +1000

    finish quiz 7

---
+++ Week8/quiz_7.py
@@ -0,0 +1,13 @@
+o,(fn"	C"GbM%cA.GiIh?q=)hTE[l&TlSCDx#'m6E?6HG6EU?0B"gI)/0i,7LUZ6<s{2Cm`Ki)pC?6HG^nCgn&Y'XC'6<I^nC?6HG^ST9oo<^9Cm8HG^nS2oHA>RTdABA!ST9ojp`nC?6HG^n'2C/+gnUUY)"gTd;BY%84-$[MY\2C;BY%84.$[MY\
+c[8W%3S(?M#,d6E?6HG6EU?0B"gI)/0i,7LUZ=mV7N)fnt
+hI#h;jlkAS6D-s{4F\<qI^nC?6HG^ST9ooV7N)fntV7N)fnJG^nC?6HGlE!3[,T6I*g`,T6I*g8HG^nC?6H,7LU"j,X39%6=-XeFjfjX"nSfgn&Y'RP1r/,`nC?6XX8nw/AY)khwVBY%8
+l[6HG^nC?6,XmU'b6ZgwI(8;D,7LU"lY'mRTOAFl^j(2uZnkAS6D-p'sU=<hI^nC?mYY^I#hn,,7C)g;-XeFh1r-$>se$8HG^nC?6HgZ E?6HG^nC?F"XgnS6B.TgCTVxLlhse?{H+|1'^?H-pOC1r-$ln,cwh-^I#hn,,7C)[6HG^nC?6hgZpC?6HG^nC1r--3NR20VXmWT2wBV7N)fn-saY%cCihlE!3[WXgT'2[<m6I(80Mn5E#hAYnq
+hVBY%84R2w.+74.!m#,dhH"lY'mRT"Hji`nC?6HG^nS6B.TgCT/xZAkrwdu/,>RH'BY%84'	m#.l1S6B%A}4'	m#.lpC?6HG^nC6oHWbS)	wWX>BThFYXghR2w.+7Sp1r--3NR20)Y>RG/y&.lh'^]JG^nC?6HG^nC?AY-nR#?OU%lEE?6HG^nC?n&,76E?6HG^nC?6HG^RThD,'^>'inJG^nC1nZG5HPbpYAkAS6D-hlE!3?(Xph'	m#.l
+l[6HG^nC?6-XeFjfjX"nSobn;AkAS6D-I^nC?6HG^ST9ooTkEP'y#jlE!3[,T6I*g=-XeFjfjX"nSE?6HG6EU?jV,hRQVBY%82S6B%A}
+l[6HG^nC?6hgZpC?6HG^nC0x(WbTXcwqGgO)/r(VeUS2miXbTW2AH"gTTfBYVmn$f6((m
+E?6HG^nC?l)(kDXbj.X)ndVGMkqre#blhksifajo}0dlbn;|
+kCa
+L%	.]>=L"5HY;,L&RGW\Mj!YH!HLi(/GCbjI^nC?6HG^ cU8HG^nC?6HWbS)	wWX>BThFYXghR2w.+7So5H*(m	dgn&Y'CTbC,X'Xi1r-$>sj0n(-kEjl<mhlE!3[WXgT'2[=m6I(80Mn5E#hAYnr
+e[6HG^nC?6<K,ST9ooV7N)fno;`nC?6HG^n.]`-XeFj0n(-kEjm8HG^nC?6H;}9S6B%A}4R2w.+74-[6HG^nC?6=L,DXgtBL'CTbC,X'YE?6HG^nC?ALslE!3[,T6I*g8HG^nC?6H+}9S6B%A}4'	m#.lpC?6HG^nC6oHWbS)	wWX>BThFYXghR2w.+7SC(6UUl	']@,L"68?vYTgSChqU-^N$h0#'5L*1nXI^nC?6HG^nC?6(Xph-/l)(kDXbj.X,	-]><L"5HY;i+}3']<pL"0dVGMmqre#m#,mA#0nBU7T,2n(A5E#hAY,"pC?6HG^nC?6HGgE,/HBVhO'1r(TmEoVHLkrse#blh!RH!ALi(seX;i<}3.]<pWbS)	wWX>BThFYXghR2w.+7Se[6HG^nC?6HG^n#2FBV7N)fnt
+hI#h;<sgE,/GBVhO'1r(TmEhm`(Xph./l)(kDXbj.X"pC?6HG^nC?6HGgE,/AUWbU(';-XeFjfjX"nSg1r-$>sjfjX"nSg1r--3NR20VXmWT2wBV7N)fn-i(sE?6HG^nC?6HG^RThD,'^$XgtiV7N)fnt'7Ww0n(-kEhfjX"nSobn;AkAS6D-i`nC?6HG^nT9BYpVn"2j(,^TW	CH-pOC1r-$lnXbl&.6ESZ6,XmU'bBH-aEC
+r!Z7RC1r-$`nC?6HG^nC?6H"8n(2uZnkAS6D-G-nS6B%A}4'	m#.l6E?6HG^nC?6HG^nC?6,XmU'b6-XeFE?6HG^nC?6HG^E!gnqI^nC?6HG^nC?6HG^n'2C/+gnS6B%A}p
