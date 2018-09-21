399910e17677342c34e2938599092f8bbb37b1ec
ly989264 <ly989264@163.com>
2018-09-21 21:30:07 +1000

    add program named find_pillow.py that can find all pillows in the result_list, and also add this to the generate_latex.py program

---
+++ Assignment2/find_pillows.py
@@ -0,0 +1,13 @@
+,H3A)	?O
+@N=>G6
+ ^&\BM{ zn
+vNH
+B,_C&3Gy<|A64_xn s"5}Mh_w"4IVn\?#``G+CHQ{2^: CP2>Fn	vX5}wn;s652x$Co"H>^::F#H`Gn@sW]1t>\BPH}t>>L6^`E%	[8,XlU!h0&"lTo+8HG^nC?6HG^nC?~Kl^sh?bmG~2C\?HL%nGZ6Ml^qh?bmG}2C^	mI^nC?6HG^nC?6Hw}2C^?HL%nHZ6Ml^th?amG|2C]?HJ%nIZ6My%pC?6HG^nC?6HG^dIZ6Kl^rh?cmG}2C^?HK%nIZ6Kl^th?9mG}fh[6HG^nC?6HG^nC+cmG|2C^?HM%nHZ6Ml^sh?cmG}2C_?HM%nF-?JG^nC?6HG^nC?6xJ%nFZ6Ll^qh?9mG{2C]?HJ%nFZ6Kl^qh?9zI^nC?6HG^nC?6zI`DT36Z"gDwdr&%hW(V<qI^nC?:hg`nC?60`E,|K67`E,CCW~8^/<q$22^/{?W
+[xn
+vJ4GB=CNN2	%n2w#`GL? FX5 w){u6		w%	$8HG^nC?6H4I6C\6
+1^rCC!H H>[wW_I^nC?6HG^,>t#qG{n|F6MGH<CBX4_B.@[6HG^6'2C/+gn'2C/+gSC	6&"lTCcoH)3I'g6;"mHChqYG8O'aj.G!Xhm<HhI!4?J43x.
+wX	GM(:H68GB=CFX7Gt.=?(H`Ln<CU5~G
+E?6HGZ c[6HG^G!ckU%^RTgD&->LXgCJG^nCdr&%hWwdj#+>LXgCtw<pC?6HYhRC2jW!>R$k0#'6E-?r(GkA#4ni%7Ndfn-.eTw9r--"
+l[6HG^nC?6Z(knT	l"A5O!iv(AbNS2GH"gn'	w!X!LTb;,XlU!h0&"lTs\	ji)pC?6HG^nC?6HGbFCfn-.eTw9r--:EP0qB+hWw6wXXqfs2jW!>C$9D''>I#1n<y,9F$8HG^nC?6HG^nC?6HGbFC2jW!>R$k0#'6E-'`KGhRCVnUVah'cFB"gDTl{KG3NS?;,XlU!h0&"lTs2jW!>R$k0#'6E-!azw7AR50W(eU"b0#'6E--`tJ^O'?AY,nL)/u#,mdT	l"AkO,/r(W7Xi]	xX3CW/l)%nM#/r(W7Xu'`Li"6E?6HG^nC?6HG^nC?6HG^nX36YT5Hw0x&.fNw6wXXqno'6KGhRCVnUVahRcu/&ghXbmY;^aC\6U'6ndfn-.eTw9r--:EP0qB+hWw6wXXqfs2jW!>C$9D''>I#1n<m|fC'`HJ^O'?AY,nL)/u#,mdT	l"AkO,/r(W7Xu+nUVahRcu/&ghXbmY;&ru?`tG}
+e$8HG^nC?6HG^nC?6HG^nC?6HG^PX9u):>PP6AB%bS)"j*)7NSV;YT5Hwfx;AbNS2GmX3CW/l)%nM#/r(W7XeW8HG^n'2C/+gn%6u&(ph%	r,AeI(h8JYhRC2jW!>PP6AH"gnU6wXAiI!9x;,!
+l[6HG^P'6w.h7AR50*TbRe
