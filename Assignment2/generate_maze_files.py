795f6a723c5f0431fa840fa8e4c4b6c6ba974dfc
ly989264 <ly989264@163.com>
2018-09-26 19:50:19 +1000

    add maz

--- Assignment2/generate_maze_files.py
+++ Assignment2/generate_maze_files.py
@@ -1,4 +1,5 @@
 # THIS PROGRAM IS USED TO GENERATE DIFFERENT MAZE FILES TO TEST THE MAZE PROGRAM
+# THIS FILE IS USED FOR TESTING THE PROGRAM
 
 import os
 from random import seed,randrange
@@ -8,6 +9,7 @@ os.chdir('maze_test_file')
 # CREATE VALID MAZE FILE OF X FROM 2 TO 31, Y FROM 2 TO 41
 count=0
 for seed in range(1,6):
+    seed(seed)# PAY ATTENTION, THE FILES GENERATED BEFORE DID NOT HAVE THIS
     for x in range(2,32):
         for y in range(2,42):
             filename=f'maze_{count}.txt'
