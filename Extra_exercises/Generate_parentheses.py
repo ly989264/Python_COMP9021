fe71f60d53296b648b9a5d09f98afa82262bbabf
ly989264 <ly989264@163.com>
2018-10-27 09:36:11 +1100

    add Generate_parentheses program in the extra exercises

--- Extra_exercises/Generate_parentheses.py
+++ Extra_exercises/Generate_parentheses.py
@@ -3,3 +3,42 @@
 #
 # "((()))", "(()())", "(())()", "()(())", "()()()"
 
+def generate_parentheses(first_list,second_list,string):
+    global final_list
+    if len(first_list)==0 and len(second_list)==0:
+        final_list.append(string)
+        return
+    if len(first_list)>len(second_list):
+        return
+    if len(first_list)==len(second_list):
+        temp=''
+        for each_char in string:
+            temp+=each_char
+        temp+=first_list[0]
+        generate_parentheses(first_list[1:],second_list,temp)
+        return
+    elif len(first_list)<len(second_list) and len(first_list)!=0:
+        temp=''
+        for each_char in string:
+            temp+=each_char
+        temp+=second_list[0]
+        generate_parentheses(first_list,second_list[1:],temp)
+        temp=''
+        for each_char in string:
+            temp+=each_char
+        temp+=first_list[0]
+        generate_parentheses(first_list[1:],second_list,temp)
+        return
+    elif len(first_list)<len(second_list) and len(first_list)==0:
+        for each_element in second_list:
+            string+=each_element
+        final_list.append(string)
+        return
+
+
+n=10
+first_list=['(' for _ in range(n)]
+second_list=[')' for _ in range(n)]
+final_list=[]
+generate_parentheses(first_list,second_list,'')
+print(final_list)
